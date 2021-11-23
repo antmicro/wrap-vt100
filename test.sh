65afd64b3fb4d6391244be589f53507572b3458d
Peter Gielda <pgielda@antmicro.com>
2021-11-23 11:47:01 +0100

    Merge two examples into one

--- test.sh
+++ test.sh
@@ -1,1 +1,3 @@
 ls --color=always | cargo run --quiet | cargo run --example line-appender --quiet -- "aaa " " bbb"
+
+echo -e '\x1b[1;31mThis red\nbold text\nspawns many lines' | cargo run --quiet | cargo run --example line-appender --quiet -- "aaa " " bbb"
