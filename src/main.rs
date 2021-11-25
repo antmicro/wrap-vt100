c0c55e9738f8b407387108cdbe0c96fde91c4205
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-11-25 17:22:01 +0100

    Distinguish background and foreground escape codes, save them in a struct. When a new escape code in the same category appears, override it and forget previous code, which is no longer useful.
    This solution is incremental, all states other than background and foreground
    are still kept in a naive vector remembering all codes.

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,66 @@ use ansi_parser::{AnsiParser, Output};
 
 const RESET_CODE: &str = "\x1b[0m";
 
+struct EscapeCodeState {
+    foreground: Option<AnsiSequence>,
+    background: Option<AnsiSequence>,
+    other: Vec<AnsiSequence>,
+}
+
+impl EscapeCodeState {
+    fn new() -> Self {
+        EscapeCodeState {
+            foreground: None,
+            background: None,
+            other: Vec::new(),
+        }
+    }
+
+    fn handle_ansi_sequence(&mut self, code: AnsiSequence) {
+        // Clear state vector if a reset sequence occurs
+        if code.to_string() == RESET_CODE {
+            self.foreground = None;
+            self.background = None;
+            self.other.clear();
+        } else {
+            match code {
+                AnsiSequence::SetGraphicsMode(ref vec) => match vec.len() {
+                    2 => match vec[1] {
+                        x if (30..=37).contains(&x) => {
+                            self.foreground = Some(code);
+                        }
+                        x if (40..=47).contains(&x) => {
+                            self.background = Some(code);
+                        }
+                        _ => self.other.push(code),
+                    },
+                    _ => self.other.push(code),
+                },
+                _ => self.other.push(code),
+            }
+        }
+    }
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut state = Vec::new();
+    let mut state = EscapeCodeState::new();
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         print!(
-            "{}",
+            "{}{}{}",
+            if let Some(ref foreground) = state.foreground {
+                foreground.to_string()
+            } else {
+                "".to_owned()
+            },
+            if let Some(ref background) = state.background {
+                background.to_string()
+            } else {
+                "".to_owned()
+            },
             state
+                .other
                 .iter()
                 .map(|code: &AnsiSequence| code.to_string())
                 .collect::<Vec<_>>()
@@ -27,13 +79,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Output::TextBlock(text) => print!("{}", text),
                 Output::Escape(code) => {
                     print!("{}", code);
-
-                    // Clear state vector if a reset sequence occurs
-                    if code.to_string() == RESET_CODE {
-                        state.clear()
-                    } else {
-                        state.push(code);
-                    }
+                    state.handle_ansi_sequence(code);
                 }
             }
         }
